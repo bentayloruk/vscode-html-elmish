c99ad0149a538655820bc2943c183ea65e29a7e6
Ben Taylor <ben.taylor.uk@gmail.com>
2017-07-12 21:35:50 +0100

    Hack HTML>Elm to HTML>Elmish.
    * class to ClassName.
    * Other attrs uppercase first char.
    * Remove use of commas in lists.
    * Change line breaking in lists to Fable React style.

---
+++ src/parser.ts
@@ -0,0 +1,75 @@
+'use strict';
+
+import {Parser} from 'htmlparser2';
+import * as utils from './utils';
+
+
+if (!String.prototype.trim) {
+    (function () {
+        let rtrim = /^[\s\uFEFF\xA0]+|[\s\uFEFF\xA0]+$/g;
+        String.prototype.trim = function () {
+            return this.replace(rtrim, '');
+        }
+    })();
+}
+
+
+export function convert(html: string, options: { indent: { with: string, size: number } }): string {
+    let elm = '';
+    let indentSize = options.indent.size;
+    let indentWith = options.indent.with;
+    let depth = -1;
+    let context = [];
+    let onOpenTag = (name: string, attributes: any): void => {
+        let pre = '';
+        let tag = [];
+        let attrs = Object.keys(attributes).map((attribute) => {
+            let value = attributes[attribute];
+            if (attribute === 'style') {
+                return attribute + ' [ ' + utils.styleToElm(value).join(', ') + ' ]';
+            }
+            if (attribute === 'class') {
+                attribute = 'ClassName';
+            }
+            attribute = attribute.charAt(0).toUpperCase() + attribute.slice(1);
+            return attribute + ' "' + value + '"';
+        });
+
+        depth++;
+        if (context[depth]) {
+            pre = '\n' + utils.indent(depth, indentSize, indentWith);
+        } else {
+            context[depth] = true;
+        }
+        tag.push(pre + name);
+        tag.push(' [');
+        if (attrs.length) {
+            tag.push(' ' + attrs.join('; ') + ' ');
+        }
+        tag.push('] [\n' + utils.indent(depth + 1, indentSize, indentWith));
+        elm += tag.join('');
+    };
+    let onText = (rawText: string): void => {
+        let text = rawText.trim();
+        if (text.length !== 0) {
+            elm += ` str "${text}" `;
+        }
+    };
+    let onCloseTag = () => {
+        if (context[depth + 1]) {
+            delete context[depth + 1];
+            elm += '\n' + utils.indent(depth + 1, indentSize, indentWith);
+        }
+        depth--;
+        elm += ']';
+    };
+    let parser = new Parser({
+        onopentag: onOpenTag,
+        ontext: onText,
+        onclosetag: onCloseTag
+    }, { decodeEntities: true });
+
+    parser.write(html);
+    parser.end('');
+    return elm;
+}
---
+++ src/utils.ts
@@ -0,0 +1,13 @@
+'use strict';
+
+export function indent(depth: number, size?: number, str?: string): string {
+    return (str || ' ').repeat(depth * (size || 4));
+}
+
+export function styleToElm(value: string): string[] {
+    let valueArray = value.split(/\s*;+\s*/g).filter((s) => !!s.length);
+    return valueArray.map((s) => {
+        let arr = s.split(/\s*:\s*(.+)/).filter((s) => !!s.length);
+        return '( "' + arr.join('", "') + '" )';
+    });
+}
